c0b3b49ea9aaed2f45c58d8a8d63ed7a9884b963
Andrew Groe <43552026+AndrewGroe@users.noreply.github.com>
2019-09-26 01:09:09 -0400

    fix pagination

--- app/components/post-list.js
+++ app/components/post-list.js
@@ -6,31 +6,47 @@ export default Component.extend({
     loading: true,
     posts: null,
     currentPage: null,
+
     init () {
         this._super(...arguments);
         this.getFirstPage();
     },
+
     getFirstPage () {
         return this.get('ajax').request('/posts?_start=0&_limit=10').then((result) => {
             console.log(result);
             this.set('posts', result);
+            this.set('currentPage', 0);
         }).catch((err) => {
             console.log(err)
         });
     },
-    getNextPage (currentPage) {
-        return this.get('ajax').request('/posts?_start=0&_limit=10').then((result) => {
-            console.log(result);
-        }).catch((err) => {
-            console.log(err)
-        });
+    getNextPage () {
+        if (this.currentPage < 9) {
+            const start = (this.currentPage + 1) * 10;
+            console.log(start);
+            return this.get('ajax').request('/posts?_start=' + start + '&_limit=10').then((result) => {
+                console.log(result);
+                this.set('posts', result);
+                this.set('currentPage', this.currentPage + 1);
+            }).catch((err) => {
+                console.log(err)
+            });
+
+        }
     },
-    getPrevPage (currentPage) {
-        return this.get('ajax').request('/posts?_start=0&_limit=10').then((result) => {
-            console.log(result);
-        }).catch((err) => {
-            console.log(err)
-        });
+    getPrevPage () {
+        if (this.currentPage > 0) {
+            const start = (this.currentPage - 1) * 10;
+            console.log(start);
+            return this.get('ajax').request('/posts?_start=' + start + '&_limit=10').then((result) => {
+                console.log(result);
+                this.set('posts', result);
+                this.set('currentPage', this.currentPage - 1);
+            }).catch((err) => {
+                console.log(err)
+            });
+        }
     },
     getUser (userId) {
         return this.get('ajax').request('/users/' + userId).then((result) => {
