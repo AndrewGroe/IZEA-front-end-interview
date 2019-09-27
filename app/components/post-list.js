451370faa03d8620620454358739b089d4cfedc1
Andrew Groe <43552026+AndrewGroe@users.noreply.github.com>
2019-09-26 21:02:13 -0400

    update styles

--- app/components/post-list.js
+++ app/components/post-list.js
@@ -1,14 +1,18 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object'
 
 export default Component.extend({
     ajax: service(),
     posts: null,
     user: null,
-    currentPage: null,
     selectedPost: null,
     loading: true,
     modal: false,
+    currentPage: null,
+    fixedPage: computed('currentPage', function () {
+        return this.currentPage + 1;
+    }),
 
     init () {
         this._super(...arguments);
@@ -16,10 +20,12 @@ export default Component.extend({
     },
 
     getFirstPage () {
+        this.set('loading', true);
         return this.get('ajax').request('/posts?_start=0&_limit=10').then((result) => {
             console.log(result);
             this.set('posts', result);
             this.set('currentPage', 0);
+            this.set('loading', false);
         }).catch((err) => {
             console.log(err)
         });
@@ -27,12 +33,14 @@ export default Component.extend({
     actions: {
         getNextPage () {
             if (this.currentPage < 9) {
+                this.set('loading', true);
                 const start = (this.currentPage + 1) * 10;
                 console.log(start);
                 return this.get('ajax').request('/posts?_start=' + start + '&_limit=10').then((result) => {
                     console.log(result);
                     this.set('posts', result);
                     this.set('currentPage', this.currentPage + 1);
+                    this.set('loading', false);
                 }).catch((err) => {
                     console.log(err);
                 });
@@ -41,23 +49,27 @@ export default Component.extend({
         },
         getPrevPage () {
             if (this.currentPage > 0) {
+                this.set('loading', true);
                 const start = (this.currentPage - 1) * 10;
                 console.log(start);
                 return this.get('ajax').request('/posts?_start=' + start + '&_limit=10').then((result) => {
                     console.log(result);
                     this.set('posts', result);
                     this.set('currentPage', this.currentPage - 1);
+                    this.set('loading', false);
                 }).catch((err) => {
                     console.log(err);
                 });
             }
         },
         getUser (index) {
+            this.set('loading', true);
             this.set('selectedPost', this.posts[index]);
             return this.get('ajax').request('/users/' + this.selectedPost.userId).then((result) => {
                 console.log(result);
                 this.set('user', result);
                 this.set('modal', true);
+                this.set('loading', false);
             }).catch((err) => {
                 console.log(err);
             });
